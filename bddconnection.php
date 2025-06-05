4d5dc30d3e81a8d479baf995a29e4550f5396209
olisld <olivier.soulard4@gmail.com>
2025-06-05 11:13:45 +0200

    correction config

--- bddconnection.php
+++ bddconnection.php
@@ -7,13 +7,13 @@
 
 // Local
     
-    $user = 'olivier';
-    $pass = 'olivier';
+    // $user = 'olivier';
+    // $pass = 'olivier';
 
 // VPS
-    // $host='57.128.221.33';
-    // $user = 'olivier';
-    // $pass = 'Baumette49000!';
+    $host='57.128.221.33';
+    $user = 'olivier';
+    $pass = 'Baumette49000!';
 
     $charset = 'utf8mb4';
     $options = [
